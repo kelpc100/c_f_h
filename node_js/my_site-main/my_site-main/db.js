a30bf143bcdba33e6f77a088b8b7ae48b7192989
kelpc100 <123337525+kelpc100@users.noreply.github.com>
2025-01-01 23:55:00 -0500

    Update db.js

--- node_js/my_site-main/my_site-main/db.js
+++ node_js/my_site-main/my_site-main/db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2")
 
 var db = mysql.createConnection({
     host: '127.0.0.1',
-    database: 'resume',
-    user: 'tree',
+    database: 'db',
+    user: 'user',
     password: 'password'
 })
 
@@ -11,8 +11,8 @@ module.exports = db;
 
 // ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 // SELECT User FROM mysql.user;
-// use resume;
+// use db;
 // show tables;
 //describe sql_injection;
-//CREATE USER 'tree'@'localhost' IDENTIFIED BY 'password';
-//GRANT ALL PRIVILEGES ON *.* TO 'tree'@'localhost' WITH GRANT OPTION;
+//CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
+//GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' WITH GRANT OPTION;
